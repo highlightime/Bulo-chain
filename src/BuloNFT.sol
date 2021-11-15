54e2ab8ff956407b9747d08d6e8583071f4a801b
hoiJuwon <withjuwon@gmail.com>
2021-11-15 21:15:46 +0900

    [feat] BuloNFT interface

---
+++ src/BuloNFT.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./BuloNFTInterface.sol";
+
+contract BuloNFT is ERC1155, BuloNFTInterface {
+    // TODO
+}
---
+++ src/BuloNFTInterface.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract BuloNFTStorage {
+    address public owner;
+
+    uint public tokenId;
+
+    struct GraveInfo {
+        string name;
+        string birth;
+        string message;
+        string imgUrl;
+        uint[] tokenIds;
+    }
+
+    mapping(address => GraveInfo) public graveInfoOf;
+}
+
+contract BuloNFTInterface is BuloNFTStorage {
+    function registerGrave(
+        string calldata name,
+        string calldata birth,
+        string calldata visitLog,
+        string calldata imgUrl
+    ) external returns (GraveInfo);
+}
