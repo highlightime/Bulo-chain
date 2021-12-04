326864fb4830f3d97f24fb1ae572ff608757623a
“yunobro git config --global user.email “yunoyunhyeong@gmail.com <yunoyunhyeong@gmail.com>
2021-12-04 16:49:09 +0900

    Registeration of Grave

--- src/BuloNFTInterface.sol
+++ src/BuloNFTInterface.sol
@@ -1,26 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract BuloNFTStorage {
-    address public owner;
-
-    uint public tokenId;
-
     struct GraveInfo {
         string name;
         string birth;
-        string message;
-        string imgUrl;
-        uint[] tokenIds;
+        uint256 tokenId; 
     }
 
+    GraveInfo[] public Graves;
+    
+    mapping (uint => address) public graveToOwner;
     mapping(address => GraveInfo) public graveInfoOf;
 }
 
-contract BuloNFTInterface is BuloNFTStorage {
-    function registerGrave(
-        string calldata name,
-        string calldata birth,
-        string calldata visitLog,
-        string calldata imgUrl
-    ) external returns (GraveInfo);
+abstract contract BuloNFTInterface is BuloNFTStorage {
+    function registerGrave(string calldata name,string calldata birth, string calldata _uri,uint _tokenId) external virtual;
 }
